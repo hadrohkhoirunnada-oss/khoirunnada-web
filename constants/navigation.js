f45a6a75934a653c186a30c72b8d0c92f39b8906
Hadroh Khoirunnada <hadrohkhoirunnada@gmail.com>
2026-06-11 15:08:40 +0800

    menghidup kan sidebar shop

--- constants/navigation.js
+++ constants/navigation.js
@@ -1,7 +1,7 @@
 export const PUBLIC_NAVIGATION = [
   { label: "Beranda", href: "/" },
   { label: "Profil & Sejarah", href: "/profil", isLocked: true },
-  { label: "Shop & Katalog", href: "/shop", isLocked: true },
+  { label: "Shop & Katalog", href: "/shop", isLocked: false },
   { label: "Qosidah & Wirid", href: "/lirik" },
   { label: "Galeri", href: "/galeri" },
   { label: "Booking", href: "/booking" },
