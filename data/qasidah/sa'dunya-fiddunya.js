bbe44bfbc9bbfeb28b740f5f3239e6aed2ec1183
Hadroh Khoirunnada <hadrohkhoirunnada@gmail.com>
2026-06-14 17:18:41 +0800

    update qosidah dikit

--- data/qasidah/sa'dunya-fiddunya.js
+++ data/qasidah/sa'dunya-fiddunya.js
@@ -6,13 +6,32 @@ export const sad_dunya_fiddunya = {
   qasidahCategory: "qosidah-umum",
   shortDescription: 
     "Sa'dunya Fiddunya berisi tentang kebahagiaan di dunia dan akhirat.",
+
   arabicText: [
-    "Sa'du dun ya fid dun ya wa sa'du uqba"
+    "سَعِدْنَا بِالدُّنْيَا ۞ فَوْزُنَا بِالْأُخْرَى",
+    "بِخَدِيجَةَ الْكُبْرَى ۞ وَفَاطِمَةَ الزَّهْرَا",
+    "سَعِدْنَا بِالدُّنْيَا ۞ فَوْزُنَا بِالْأُخْرَى",
+    "بِخَدِيجَةَ الْكُبْرَى ۞ وَفَاطِمَةَ الزَّهْرَا",
+    "بِخَدِيجَةَ الْكُبْرَى ۞ وَفَاطِمَةَ الزَّهْرَا",
+    "يَا أُهَيْلَ الْمَعْرُوفْ ۞ وَالْعَطَا وَالْمَأْلُوفْ",
+    "غَارَةً لِلْمَلْهُوفْ ۞ إِنَّكُمْ بِهِ أَدْرَى",
   ],
   latinText: [
-    "Sa'du dun ya fid dun ya wa sa'du uqba"
+    "Sa'duna fid-dunya ۞ Fawzuna bil-ukhro",
+    "Bi Khodijatal Kubro ۞ Wa Fathimataz Zahro",
+    "Sa'duna fid-dunya ۞ Fawzuna bil-ukhro",
+    "Bi Khodijatal Kubro ۞ Wa Fathimataz Zahro",
+    "Bi Khodijatal Kubro ۞ Wa Fathimataz Zahro",
+    "Ya uhailal ma'ruf ۞ Wal 'atha wal ma'luf",
+    "Ghorotan lil malhuf ۞ Innakum bihi adro",
   ],
   translation: [
-    "Sa'du dun ya fid dun ya wa sa'du uqba"
+    "Kebahagiaan kami di dunia ۞ Keberuntungan kami di akhirat",
+    "Dengan perantara Khodijah al Kubro ۞ Dan Fathimah az Zahro",
+    "Kebahagiaan kami di dunia ۞ Keberuntungan kami di akhirat",
+    "Dengan perantara Khodijah al Kubro ۞ Dan Fathimah az Zahro",
+    "Dengan perantara Khodijah al Kubro ۞ Dan Fathimah az Zahro",
+    "Wahai pemilik kebaikan ۞ Dan pemberian yang disukai",
+    "Berikanlah kepada orang yang berduka ۞ Sungguh kalian lebih mengerti dirinya",
   ],
 };
